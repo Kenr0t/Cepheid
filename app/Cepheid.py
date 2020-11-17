497d6c34a99d6d2d9d91e5c4c083c19796c66d9e
Kenr0t <kenrot.shooter@gmail.com>
2020-11-17 17:31:25 +0000

    Added Logger to Cepheid.py. Added requirements.txt.
    Others minors changes.

--- app/Cepheid.py
+++ app/Cepheid.py
@@ -1,31 +1,38 @@
 import json
-from logging import Logger
+import logging
 
 from flask import request, Response
 
 from Rules_db import RulesDB, Rule
-from config import default_service, default_servicepath
+from config import default_service, default_servicepath, CEP_MONGO_HOST
+
+logger = logging.getLogger(__name__)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
 
 class Cepheid:
     instance = None
-
     rules = None
-    set_de_reglas = None
 
     def __new__(cls):
         if cls.instance is None:
+            logger.info('Connecting to database...')
             cls.rules_db = RulesDB()
-
+            logger.info(f'Conected to MongoDB (Host: {CEP_MONGO_HOST})')
             cls.instance = object.__new__(cls)
         return cls.instance
-
 
     def ejecutar_reglas(self, evaluate_only=False):
         rules = [r for svc, svcP in self.rules_db.get_services() for r in self.rules_db.get_all(svc, svcP)]
 
         to_do = Rule.eval if evaluate_only else Rule.execute
         for r in rules:
-            print(r.rule, '-->', to_do(r))
+            logger.info(f'{r.rule} --> {to_do(r)}')
 
     def setup_notifiaciones(self, app):
         @app.route('/notify', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
@@ -34,22 +41,31 @@ class Cepheid:
             servicePath = request.headers['Fiware-ServicePath']
             if request.data and 'subscriptionId' in request.json:
                 datos = request.json
+                logger.info(f'Notification from a Subscription received. Subs. Id: {datos["subscriptionId"]}')
                 rule = self.rules_db.find_by_subscription_id(datos['subscriptionId'], service, servicePath)
                 rule.execute()
                 return Response(status=200)
+            elif request.data:
+                logger.error(f'No subscriptionId in the request. {json.dumps(request.data, indent=4)}')
+            else:
+                logger.error(f'No data in the request.')
             return Response(status=404)
 
     def setup_crud(self, app):
         @app.route('/rules', methods=['POST'])
         def insert_rules():
             if not request.is_json:
+                logger.error('Not json data in the content.')
                 return Response(
-                    '{"error": "UnsupportedMediaType", "description": "not supported content type: text/plain"}',
-                    status=415, headers={"Content-Type": "application/json"})
+                    '{"error": "UnsupportedMediaType", "description": "Not json data in the content"}',
+                    status=415, headers={"Content-Type": "application/json"}
+                )
             if 'Fiware-Service' not in request.headers or 'Fiware-ServicePath' not in request.headers:
+                logger.error('Not supported content type: text/plain')
                 return Response(
                     '{"error": "UnsupportedMediaType", "description": "not supported content type: text/plain"}',
-                    status=400, headers={"Content-Type": "application/json"})
+                    status=400, headers={"Content-Type": "application/json"}
+                )
             try:
                 data = request.json
                 data['service'] = request.headers.get('Fiware-Service', default_service)
@@ -57,19 +73,25 @@ class Cepheid:
                 r = Rule.from_dict(data)
             except TypeError:
                 err = '{"error": "ParseError", "description": "Errors found in incoming JSON buffer"}'
+                logger.error('Errors found in incoming JSON buffer.')
                 return Response(json.dumps(err), status=400, content_type='application/json')
             except Exception as e:
+                logger.error(f'Error: {e}')
                 err = {"error": "ParseError", "description": str(e)}
                 return Response(json.dumps(err), status=400, content_type='application/json')
             if r not in self.rules_db:
+
                 r.subscribe()
                 rule_id = self.rules_db.insert(r)
                 if rule_id is None:
                     r.unsubscribe()
+                    logger.info(f'Rule cannot be inserted: {json.dumps(r.to_dict(), indent=4)}')
                     err = '{"error": "UnknownError", "description": "Something happened while inserting the rule"}'
                     return Response(json.dumps(err), status=500, content_type='application/json')
+                logger.info(f'Rule inserted: {json.dumps(r.to_dict(), indent=4)}')
                 return Response(status=200, headers={'Location': f'/rules/{rule_id}'})
             else:
+                logger.warning(f'This rule already exists. Rule: {json.dumps(r.to_dict(), indent=4)}')
                 err_msg = {
                     "error": "Already Exists",
                     "description": "The rule you are trying to insert already exitsts in the database."
@@ -83,13 +105,18 @@ class Cepheid:
             servicepath = request.headers.get('Fiware-ServicePath', default_servicepath)
 
             if rule_id is None:  # Return all the rules
-                return Response(json.dumps(self.rules_db.get_all(service, servicepath, in_json=True)), status=200,
-                                content_type='application/json')
+                logger.info('Returning all the Rules.')
+                return Response(
+                    json.dumps(self.rules_db.get_all(service, servicepath, in_json=True)),
+                    status=200, content_type='application/json'
+                )
             else:
                 rule = self.rules_db.find_by_id(rule_id, service, servicepath, in_json=True)
                 if rule:
+                    logger.info(f'Returning the rule with id: {rule_id}.')
                     return Response(json.dumps(rule), status=200, content_type='application/json')
                 else:
+                    logger.warning(f'No rule with id {rule_id}.')
                     err_not_found = {
                         "error": "NotFound", "description": "The requested rule has not been found. Check id."
                     }
@@ -102,8 +129,10 @@ class Cepheid:
 
             result = self.rules_db.delete_by_id(rule_id, service, servicepath)
             if result:
+                logger.info(f'Deleting the rule with id: {rule_id}.')
                 return Response(status=204, content_type='application/json')
             else:
+                logger.warning(f'No rule with id {rule_id}.')
                 err_not_found = {
                     "error": "NotFound", "description": "The requested rule has not been found. Check id."
                 }
--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
 
+
 CEP_CB_HOST = os.getenv('CEP_CB_HOST', 'localhost')
 CEP_CB_PORT = os.getenv('CEP_CB_PORT', '1026')
 CEP_IOTA_HOST = os.getenv('CEP_IOTA_HOST', 'localhost')
@@ -11,7 +12,6 @@ CEP_DEFAULT_SERVICE = os.getenv('CEP_DEFAULT_SERVICE', 'orion')
 CEP_DEFAULT_SERVICEPATH = os.getenv('CEP_DEFAULT_SERVICEPATH', '/environment')
 CEP_PROVIDER_URL = os.getenv('CEP_PROVIDER_URL', 'http://0.0.0.0:4013')
 
-print(CEP_CB_HOST, CEP_IOTA_HOST, CEP_MONGO_HOST, sep='\n')
 
 default_service = CEP_DEFAULT_SERVICE
 default_servicepath = CEP_DEFAULT_SERVICEPATH
